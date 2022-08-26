83b4f4666cdfb0d52084a2077c4cec4a71871ea2
panjuanyi <mitena@126.com>
2022-08-26 21:57:42 +0800

    修改

--- js/js.js
+++ js/js.js
@@ -1,11 +1,27 @@
 
+// class太多，弄个封装的函数，获取className
+function getByClass(clsName,parent){
+    let oParent=parent?document.getElementById(parent):document
+    let oClass= oParent.querySelector(clsName)
+    return oClass
+}
+
 window.onload=function(){
-    let searchBtn=document.querySelector('.search-submit');
-    searchBtn.onclick=function (){openSearch()
-    };
+    openSearch()
+
 }
 
 function openSearch(){
-    let e=document.querySelector('.search-text').value;
-    window.open('https://www.baidu.com/s?wd='+e)
+    let seBtn=getByClass('.search-submit')
+    seBtn.onclick=bdSearch
+    document.onkeyup=function(e){
+    if(e.key=='Enter'){
+        bdSearch()
+        }
+    }  
+}
+
+function bdSearch(){
+    let kw=getByClass('.search-text').value;
+    window.open('https://www.baidu.com/s?wd='+kw)
 }
