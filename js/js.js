5f72ccc3c229c0a5a3325ca102b5745dfea92b24
panjuanyi <mitena@126.com>
2022-08-28 21:42:37 +0800

    修改

--- js/js.js
+++ js/js.js
@@ -7,21 +7,76 @@ function getByClass(clsName,parent){
 }
 
 window.onload=function(){
+    // 搜索
     openSearch()
+    // 下拉框的
+    openSubList()
 
 }
 
 function openSearch(){
     let seBtn=getByClass('.search-submit')
-    seBtn.onclick=bdSearch
+    // 鼠标事件-搜索
+    seBtn.onclick=function(e){
+    // 防止冒泡
+    e.stopPropagation()
+    bdSearch()
+    } 
+
+    // 键盘enter事件-搜索
     document.onkeyup=function(e){
     if(e.key=='Enter'){
         bdSearch()
         }
-    }  
+    }
 }
 
+// 链接到百度的函数
 function bdSearch(){
     let kw=getByClass('.search-text').value;
     window.open('https://www.baidu.com/s?wd='+kw)
+}
+
+// 判定文本框内内容，确定展开下拉菜单
+// emm，设个定时器？？？
+let timer=null;
+
+function openSubList(){
+    clearInterval (timer);
+    timer=setInterval(function() {
+        // 看看搜索框里的文字
+        let kw=getByClass('.search-text').value;
+
+        // 对清除和下拉搜索框声明个变量，方便后续操作
+        let sc=getByClass('.search-clear');
+        let ssl=getByClass('.search-sub-list');
+
+        // 设个判定条件-旗帜，默认false：隐藏状态
+        let flag=false;
+
+        // 有字则下拉，没字就隐藏
+        if(kw.length==0){
+        flag=false
+        }
+        else{
+        flag=true
+        }
+
+        //如果在空白处点击，则隐藏
+        document.onclick = function () {
+        flag=false
+        };
+        
+        console.log(kw.length,flag)
+        // 为什么这边flag一直是true啊？？？
+
+        if(flag==false){
+        sc.style.display = "none";
+        ssl.style.display = "none";
+        }
+        else{
+        sc.style.display = "block";
+        ssl.style.display = "block";
+        }
+    }, 30);
 }
